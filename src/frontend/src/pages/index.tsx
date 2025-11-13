8e55d014c24e4dab6fe3fdf689ee4ea03880b0c5
alevale <alejandro@mosa.cloud>
2025-11-14 00:04:49 +0100

    overwrite styles and make the login button ours

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { GlobalLayout } from "@/features/layouts/components/global/global-layout";
 import { useTranslation } from "react-i18next";
-import { Hero, HomeGutter, Footer, ProConnectButton } from "@gouvfr-lasuite/ui-kit";
+import { Hero, HomeGutter, Footer } from "@gouvfr-lasuite/ui-kit";
 import { login, useAuth } from "@/features/auth";
 import { MainLayout } from "@/features/layouts/components/main";
 import { LanguagePicker } from "@/features/layouts/components/main/language-picker";
 import { AppLayout } from "@/features/layouts/components/main/layout";
 import { LeftPanel } from "@/features/layouts/components/main/left-panel";
+import { Button } from "@openfun/cunningham-react";
 
 export default function HomePage() {
 
@@ -21,11 +22,17 @@ export default function HomePage() {
     <div className="app__home">
       <HomeGutter>
         <Hero
-          logo={<img src="/images/app-icon.svg" alt="Messages Logo" width={64} />}
+          logo={
+            <img src="/images/app-icon.svg" alt="Messages Logo" width={64} />
+          }
           title={t("home.title")}
           banner="/images/banner.webp"
           subtitle={t("home.subtitle")}
-          mainButton={<ProConnectButton onClick={login} />}
+          mainButton={
+            <Button onClick={login}>
+              {t("home.mainButtonLogin")}
+            </Button>
+          }
         />
       </HomeGutter>
       <Footer />
