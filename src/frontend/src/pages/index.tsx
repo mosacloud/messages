1b9cd35be0caf84ceaca203c3ff5f011955351d3
daan <daan@zzapps.nl>
2026-01-15 16:22:43 +0100

    feat: replace la suite login page with the mosa login page component

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,52 +1,13 @@
-import { useTranslation } from "react-i18next";
-import { Hero, HomeGutter, Footer } from "@gouvfr-lasuite/ui-kit";
-import { login, useAuth } from "@/features/auth";
+import { useAuth } from "@/features/auth";
 import { MainLayout } from "@/features/layouts/components/main";
-import { LanguagePicker } from "@/features/layouts/components/main/language-picker";
-import { AppLayout } from "@/features/layouts/components/main/layout";
-import { LeftPanel } from "@/features/layouts/components/main/left-panel";
-import { FeedbackWidget } from "@/features/ui/components/feedback-widget";
-import { useTheme } from "@/features/providers/theme";
-import { Button } from "@gouvfr-lasuite/cunningham-react";
+import { MosaLoginPage } from "@/features/home";
 
 export default function HomePage() {
-  const { t } = useTranslation();
-  const { themeConfig } = useTheme();
   const { user } = useAuth();
 
   if (user) {
     return <MainLayout />;
   }
 
-
-  return (
-    <AppLayout
-      hideLeftPanelOnDesktop
-      leftPanelContent={<LeftPanel />}
-      rightHeaderContent={<LanguagePicker />}
-      icon={<img src={`/images/app-icon.svg`} alt="logo" height={40} />}
-    >
-      <div className="app__home">
-        <HomeGutter>
-          <Hero
-            logo={
-              <img
-                src={`/images/app-icon.svg`}
-                alt="Messages Logo"
-                width={64}
-              />
-            }
-            title={t("Simple and intuitive messaging")}
-            banner="/images/banner.webp"
-            subtitle={t("Send and receive your messages in an instant.")}
-            mainButton={
-              <Button onClick={login}>{t("Log in")}</Button>
-            }
-          />
-        </HomeGutter>
-        {themeConfig.footer && <Footer {...themeConfig.footer} />}
-      </div>
-      <FeedbackWidget />
-    </AppLayout>
-  );
+  return <MosaLoginPage />;
 }
